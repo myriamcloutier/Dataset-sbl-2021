e4862a4e66d77180d76fc6a58c6a726057db5dba
myriamcloutier <myriam.cloutier8@gmail.com>
2023-08-06 18:16:20 -0400

    Update metadata_extraction.R

--- metadata/metadata_extraction.R
+++ metadata/metadata_extraction.R
@@ -3,20 +3,47 @@
 # Here is how to use those functions to extract metadata from local tif files.
 ####
 
+library("gdalcubes")
+library("rstac")
+library("raster")
+
 ##### To extract the bounding box for a raster
 
-# Load the raster data
+# Load and read the raster data
 
-load_cube() # This function will return a proxy raster data cube
+raster_file <- "path/to/tif"
+r <- raster(raster_file)
 
-# Extract raster coordinates
 
-x <- gdalcubes::dimension_values(cube)$x
-y <- gdalcubes::dimension_values(cube)$y
-raster_coords <- expand.grid(x, y) %>% setNames(c("lon", "lat"))
+# Get the bounding box using points_to_bbox function
 
-# Calculate the bounding box
+bbox <- points_to_bbox(as.data.frame(extent(r)), buffer = 0, proj.from = projection(r))
 
-bbox <- points_to_bbox(raster_coords, proj.from = "EPSG:32198")
+# bbox <- points_to_bbox(raster_coords, proj.from = "EPSG:32198")
 
 # bbox will be a numeric vector with xmin, xmax, ymin, and ymax representing the extent of the raster's bounding box.
+
+print(bbox)
+
+
+
+#### To extract the bounding box in a loop
+
+# Function to calculate bounding box for a single raster
+get_raster_bbox <- function(raster_file) {
+  r <- raster(raster_file)
+  bbox <- points_to_bbox(as.data.frame(extent(r)), buffer = 0, proj.from = projection(r))
+  return(bbox)
+}
+
+# List of raster file paths
+raster_files <- c("path/to/1.tif", "path/to/2.tif")
+
+# Apply the function to get bounding box for each raster
+bbox_list <- lapply(raster_files, get_raster_bbox)
+
+# Combine the data frames into a single object
+all_bboxes <- do.call(rbind, bbox_list)
+
+# Print the combined bounding box data frame
+print(all_bboxes)
