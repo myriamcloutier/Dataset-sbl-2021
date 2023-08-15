3018e42034f6c3809dac493ddcfef7371781bba8
myriamcloutier <myriam.cloutier8@gmail.com>
2023-08-15 17:06:08 -0400

    Update coord_extraction.R

--- metadata/coord_extraction.R
+++ metadata/coord_extraction.R
@@ -4,4 +4,26 @@
 
 library(raster)
 
-get
+## From chatGPT, not tested
+
+get_raster_bbox <- function(raster_files) {
+  coords_list <- list()
+  
+  for (file in raster_files) {
+    r <- raster(file_test)
+    num_cells <- ncell(r)
+    xy <- xyFromCell(r, 1:num_cells)
+    coords_list[[file]] <- data.frame(x = xy[, 1], y = xy[, 2])
+  }
+  
+  return(coords_list)
+}
+
+library(readr)
+
+# Read CSV data
+raster_files <- read_csv("file_paths.csv") # name of csv
+raster_paths <- raster_files$file_path
+
+# Apply the function to get coordinates for each raster
+coords_list <- get_raster_bbox(raster_paths)
