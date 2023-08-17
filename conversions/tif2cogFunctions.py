46d80d6a530568d947fef9042c7bce61014127b5
myriamcloutier <myriam.cloutier8@gmail.com>
2023-08-17 11:18:04 -0400

    Update tif2cogFunctions.py

--- conversions/tif2cogFunctions.py
+++ conversions/tif2cogFunctions.py
@@ -47,15 +47,3 @@ def tif2cog(input_paths: List[Path], output_cog_path: Path, type: str) -> None:
 
     if not output_cog_path.exists():
         raise FileNotFoundError("The COG was not created, see the full logs for more details.")
-
-# Defining input paths
-input_paths = [
-    Path("F:\Dataset-2021-sbl\2021-05-28\zone1\2021-05-28-sbl-z1-rgb-cog.tif"),
-    Path("F:\Dataset-2021-sbl\2021-05-28\zone2\2021-05-28-sbl-z2-rgb-cog.tif"),
-    Path("F:\Dataset-2021-sbl\2021-05-28\zone3\2021-05-28-sbl-z3-rgb-cog.tif")
-]
-
-# Defining output path
-output_cog_path = Path("F:\Dataset-2021-sbl\2021-05-28\zone1\2021-05-28-sbl-z1-rgb-cog-OUTPUT.tif")
-
-tif2cog(input_paths, output_cog_path, type="raw")
