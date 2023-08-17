a8d01b2fa5e3ef4c08efb18506773c94366025e7
myriamcloutier <myriam.cloutier8@gmail.com>
2023-08-17 11:18:00 -0400

    Create run_command.py

---
+++ conversions/run_command.py
@@ -0,0 +1,21 @@
+from subprocess import run, CalledProcessError, CompletedProcess
+from typing import List
+
+
+def run_command(command: List, timeout: int=600) -> CompletedProcess:
+    """Run a subprocess command with error handling and logging functionality.
+    """
+    process = run(command, capture_output=True, timeout=timeout)
+
+    try:
+        process.check_returncode()
+        # Log successful command...
+        return process
+
+    except CalledProcessError as e:
+        ...
+        # Send (process.returncode, process.stderr) to logging.
+        if which(command[0]) is None:
+            raise RuntimeError(f"{command[0]} was not found, is it installed and on the path?")
+        else:
+            raise ...
