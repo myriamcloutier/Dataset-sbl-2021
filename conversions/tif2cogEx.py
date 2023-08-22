11d964a9a002a4d77c216e4d5ae442d54f2c86f4
myriamcloutier <myriam.cloutier8@gmail.com>
2023-08-22 16:07:05 -0400

    Update tif2cogEx.py

--- conversions/tif2cogEx.py
+++ conversions/tif2cogEx.py
@@ -5,11 +5,13 @@ from tif2cog import tif2cog
 
 # Defining input paths
 # CHANGE TO PATH IN LEFODATA
-input_paths = [
-    Path("F:\Dataset-2021-sbl\\2021-05-28\zone1\\2021-05-28-sbl-z1-rgb-cog.tif"),
-    Path("F:\Dataset-2021-sbl\\2021-05-28\zone2\\2021-05-28-sbl-z2-rgb-cog.tif"),
-    Path("F:\Dataset-2021-sbl\\2021-05-28\zone3\\2021-05-28-sbl-z3-rgb-cog.tif")
-]
+#input_paths = [
+ #   Path("F:\Dataset-2021-sbl\\2021-05-28\zone1\\2021-05-28-sbl-z1-rgb-cog.tif"),
+  #  Path("F:\Dataset-2021-sbl\\2021-05-28\zone2\\2021-05-28-sbl-z2-rgb-cog.tif"),
+   # Path("F:\Dataset-2021-sbl\\2021-05-28\zone3\\2021-05-28-sbl-z3-rgb-cog.tif")
+#]
+
+input_paths = [Path("F:\Dataset-2021-sbl\\2021-05-28\zone3\\2021-05-28-sbl-z3-rgb-cog.tif")]
 
 #input_paths = [
 #    Path("\\lefodata\home\data-MSc-cloutier-2021\orthomosaics\quebec_tree_dataset\2021-05-28\2021-05-28-sbl-cloutier-z1-UTM18-MS"),
@@ -18,7 +20,7 @@ input_paths = [
 #]
 
 # Defining output path
-output_cog_path = Path("F:\Dataset-2021-sbl\\2021-05-28\zone1\\2021-05-28-sbl-z1-rgb-cog-OUTPUT.tif")
+output_cog_path = Path("F:\Dataset-2021-sbl\\2021-05-28\zone3\\2021-05-28-sbl-z3-rgb-cog-OUTPUT.tif")
 
 tif2cog(input_paths, output_cog_path, type="raw")
 
