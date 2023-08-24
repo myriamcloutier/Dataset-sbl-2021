75d7c578ea827dc317d8b5f71af2a7f90f378cbc
myriamcloutier <myriam.cloutier8@gmail.com>
2023-08-24 10:43:23 -0400

    Update tif2cogmodif.py

--- conversions/tif2cogmodif.py
+++ conversions/tif2cogmodif.py
@@ -8,13 +8,13 @@ import os
 
 def tif2cog(input_paths: List[Path], type: str, value_type: str="category") -> None:
     for input_path in input_paths:
-        output_cog_path = input_path.with_suffix("_OUTPUT.tif")
+        output_cog_path = input_path.with_suffix(".cog.tif")
 
         temp_vrt_path = input_path
     
         if (type=='raw'): 
             #cog_command = ['gdalwarp', '-of', 'COG', '-co', 'COMPRESS=DEFLATE', '-co', 'OVERVIEWS=IGNORE_EXISTING','-wo','NUM_THREADS=4', '-co', 'NUM_THREADS=ALL_CPUS','-multi', temp_vrt_path, output_cog_path]
-            cog_command= "gdalwarp -of COG -co COMPRESS=DEFLATE -co NUM_THREADS=ALL_CPUS -co OVERVIEWS=IGNORE_EXISTING -wo NUM_THREADS=4 -multi  %s %s" % (temp_vrt_path, output_cog_path)
+            cog_command= "gdalwarp -of COG -co COMPRESS=LZW -co BIGTIFF=YES -co NUM_THREADS=ALL_CPUS -co OVERVIEWS=IGNORE_EXISTING -wo NUM_THREADS=4 -multi  %s %s" % (temp_vrt_path, output_cog_path)
         elif (type=='display'):
             # cog_command = ['gdalwarp', '-of', 'COG', '-s_srs', 'EPSG:32198', '-t_srs', 'EPSG:3857', '-tr', '90 90', '-co', 'TILING_SCHEME=GoogleMapsCompatible', '-co', 'COMPRESS=DEFLATE', '-co', 'NUM_THREADS=ALL_CPUS','-co', 'OVERVIEWS=IGNORE_EXISTING', '-wo','NUM_THREADS=4','-multi', temp_vrt_path, output_cog_path]
             cog_command= "gdalwarp -of COG -t_srs EPSG:3857 -co COMPRESS=DEFLATE -co LEVEL=6 -co NUM_THREADS=ALL_CPUS -co OVERVIEWS=IGNORE_EXISTING -wo NUM_THREADS=4 -multi  %s %s" % (temp_vrt_path, output_cog_path)
@@ -25,3 +25,6 @@ def tif2cog(input_paths: List[Path], type: str, value_type: str="category") -> N
 
         if not output_cog_path.exists():
             raise FileNotFoundError("The COG was not created, see the full logs for more details.")
+        
+
+#"gdalwarp -of COG -co COMPRESS=DEFLATE -co NUM_THREADS=ALL_CPUS -co OVERVIEWS=IGNORE_EXISTING -wo NUM_THREADS=4 -multi  %s %s" % (temp_vrt_path, output_cog_path)
