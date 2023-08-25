d5d7021548d6457cb442fbf6057ed66b63bf899a
myriamcloutier <myriam.cloutier8@gmail.com>
2023-08-25 13:33:31 -0400

    Update tif2cogmodif.py

--- conversions/tif2cogmodif.py
+++ conversions/tif2cogmodif.py
@@ -6,15 +6,16 @@ import tempfile
 from run_command import run_command
 import os
 
-def tif2cog(input_paths: List[Path], type: str, value_type: str="category") -> None:
+def tif2cog(input_paths: List[Path], suffix: str, type: str, value_type: str="category") -> None:
     for input_path in input_paths:
-        output_cog_path = input_path.with_suffix(".cog.tif")
+        print(f"Converting: {input_path}")
+        output_cog_path = input_path.with_name(f"{input_path.stem}-{suffix}{input_path.suffix}")
 
         temp_vrt_path = input_path
     
         if (type=='raw'): 
             #cog_command = ['gdalwarp', '-of', 'COG', '-co', 'COMPRESS=DEFLATE', '-co', 'OVERVIEWS=IGNORE_EXISTING','-wo','NUM_THREADS=4', '-co', 'NUM_THREADS=ALL_CPUS','-multi', temp_vrt_path, output_cog_path]
-            cog_command= "gdalwarp -of COG -co COMPRESS=LZW -co BIGTIFF=YES -co NUM_THREADS=ALL_CPUS -co OVERVIEWS=IGNORE_EXISTING -wo NUM_THREADS=4 -multi  %s %s" % (temp_vrt_path, output_cog_path)
+            cog_command= "gdalwarp -of COG -co COMPRESS=DEFLATE -co NUM_THREADS=ALL_CPUS -co OVERVIEWS=IGNORE_EXISTING -wo NUM_THREADS=4 -multi  %s %s" % (temp_vrt_path, output_cog_path)
         elif (type=='display'):
             # cog_command = ['gdalwarp', '-of', 'COG', '-s_srs', 'EPSG:32198', '-t_srs', 'EPSG:3857', '-tr', '90 90', '-co', 'TILING_SCHEME=GoogleMapsCompatible', '-co', 'COMPRESS=DEFLATE', '-co', 'NUM_THREADS=ALL_CPUS','-co', 'OVERVIEWS=IGNORE_EXISTING', '-wo','NUM_THREADS=4','-multi', temp_vrt_path, output_cog_path]
             cog_command= "gdalwarp -of COG -t_srs EPSG:3857 -co COMPRESS=DEFLATE -co LEVEL=6 -co NUM_THREADS=ALL_CPUS -co OVERVIEWS=IGNORE_EXISTING -wo NUM_THREADS=4 -multi  %s %s" % (temp_vrt_path, output_cog_path)
@@ -25,6 +26,3 @@ def tif2cog(input_paths: List[Path], type: str, value_type: str="category") -> N
 
         if not output_cog_path.exists():
             raise FileNotFoundError("The COG was not created, see the full logs for more details.")
-        
-
-#"gdalwarp -of COG -co COMPRESS=DEFLATE -co NUM_THREADS=ALL_CPUS -co OVERVIEWS=IGNORE_EXISTING -wo NUM_THREADS=4 -multi  %s %s" % (temp_vrt_path, output_cog_path)
