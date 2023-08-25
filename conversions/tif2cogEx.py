3f2b115850fd7382258d80717227f0ab0942ef11
myriamcloutier <myriam.cloutier8@gmail.com>
2023-08-25 13:33:28 -0400

    Update tif2cogEx.py

--- conversions/tif2cogEx.py
+++ conversions/tif2cogEx.py
@@ -1,27 +1,26 @@
-from pathlib import Path # provides support for working with file paths
-#from tif2cogFonctionGL import tif2cogGL, run_command
-#from tif2cog import tif2cog
+from pathlib import Path
+import csv
 from tif2cogmodif import tif2cog
 
-# Defining input paths
-# CHANGE TO PATH IN LEFODATA
-input_paths = [
-    Path("F:\Dataset-2021-sbl\\2021-05-28\zone1\\2021-05-28-sbl-z1-rgb-cog.tif"),
-    Path("F:\Dataset-2021-sbl\\2021-05-28\zone2\\2021-05-28-sbl-z2-rgb-cog.tif"),
-    Path("F:\Dataset-2021-sbl\\2021-05-28\zone3\\2021-05-28-sbl-z3-rgb-cog.tif")
-]
+# Defining input paths from CSV file
+
+input_paths = []
+
+with open('geotiff_path.csv', 'r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+    next(csv_reader)  # Skip the header row
+    for row in csv_reader:
+        input_paths.append(Path(row[0]))
+
 
 #input_paths = [
-#    Path("\\lefodata\home\data-MSc-cloutier-2021\orthomosaics\quebec_tree_dataset\2021-05-28\2021-05-28-sbl-cloutier-z1-UTM18-MS"),
- #   Path("\\lefodata\home\data-MSc-cloutier-2021\orthomosaics\quebec_tree_dataset\2021-05-28\2021-05-28-sbl-cloutier-z2-UTM18-MS")
-  #  Path("\\lefodata\home\data-MSc-cloutier-2021\orthomosaics\quebec_tree_dataset\2021-05-28\2021-05-28-sbl-cloutier-z3-UTM18-MS")
+ #   Path("F:\Dataset-2021-sbl\\2021-05-28\zone1\\2021-05-28-sbl-z1-rgb.tif"),
+ #   Path("F:\Dataset-2021-sbl\\2021-05-28\zone2\\2021-05-28-sbl-z2-rgb.tif"),
+  #  Path("F:\Dataset-2021-sbl\\2021-05-28\zone3\\2021-05-28-sbl-z3-rgb.tif")
 #]
 
-# Defining output path
-#output_cog_path = Path("F:\Dataset-2021-sbl\\2021-05-28\zone1\\2021-05-28-sbl-z1-rgb-cog-OUTPUT.tif")
+tif2cog(input_paths, suffix="cog", type="raw")
 
-tif2cog(input_paths, type="raw")
-
-#for path in input_paths:
- #   if not path.exists():
-  #      raise FileNotFoundError(f"Input file not found: {path}")
+for path in input_paths:
+    if not path.exists():
+        raise FileNotFoundError(f"Input file not found: {path}")
