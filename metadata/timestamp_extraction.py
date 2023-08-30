19f7d205878712244136621d6235b0fbab19478e
myriamcloutier <myriam.cloutier8@gmail.com>
2023-08-30 11:28:31 -0400

    Update timestamp_extraction.py

--- metadata/timestamp_extraction.py
+++ metadata/timestamp_extraction.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 from PIL import Image 
+from datetime import datetime, timedelta
 
 def get_image_timestamp(image_path):
     try:
@@ -14,53 +15,74 @@ def get_image_timestamp(image_path):
         print(f"Error processing {image_path}: {e}")
     return None
 
-def process_file_paths(file_paths):
-    data = []
-
-    for image_path in file_paths:
-        timestamp = get_image_timestamp(image_path)
-        if timestamp:
-            data.append({"File Path": image_path, "Timestamp": timestamp})
-
-    return data
-
-#if __name__ == "__main__":
-    # Read the CSV file containing file paths CHANGE FOR CSV CONTAINING PATH TO PHOTOS, NOT ORTHOS
-    csv_file = "filenamesrgb_TS.csv"
-    df = pd.read_csv(csv_file)
-
-    # Get a list of file paths from the DataFrame
-    file_paths = df["File Path"].tolist()
-
-    # Process file paths and create a list of dictionaries
-    result_data = process_file_paths(file_paths)
-
-    # Create a DataFrame from the list of dictionaries
-    result_df = pd.DataFrame(result_data)
-
-    # Display the DataFrame
-    print(result_df)
-
-#def process_folder(folder_path):
- #   earliest_timestamp = None
-  #  latest_timestamp = None
+def process_folder(folder_path):
+    earliest_timestamp = None
+    latest_timestamp = None
     
-   # for root, _, files in os.walk(folder_path):
-    #    for file in files:
-     #       if file.lower().endswith(".jpg"):
-      #          image_path = os.path.join(root, file)
-       #         timestamp = get_image_timestamp(image_path)
+    for root, _, files in os.walk(folder_path):
+        for file in files:
+            if file.lower().endswith(".jpg"):
+                image_path = os.path.join(root, file)
+                timestamp = get_image_timestamp(image_path)
                 
-        #        if timestamp:
-         #           if not earliest_timestamp or timestamp < earliest_timestamp:
-          #              earliest_timestamp = timestamp
-           #         if not latest_timestamp or timestamp > latest_timestamp:
-            #            latest_timestamp = timestamp
+                if timestamp:
+                    # Parse the timestamp into a datetime object
+                    try:
+                        timestamp = datetime.strptime(timestamp, "%Y:%m:%d %H:%M:%S")
+                    except Exception as e:
+                        print(f"Error parsing timestamp {timestamp}: {e}")
+                        continue  # Skip this file if timestamp parsing fails
+
+                    # Apply 4-hour time offset
+                    timestamp += timedelta(hours=4)
+
+                    if not earliest_timestamp or timestamp < earliest_timestamp:
+                        earliest_timestamp = timestamp
+                    if not latest_timestamp or timestamp > latest_timestamp:
+                        latest_timestamp = timestamp
+
+                    #print(f"Processed {image_path}, Timestamp:{timestamp}")
     
-    #return earliest_timestamp, latest_timestamp
+    return earliest_timestamp, latest_timestamp
 
 if __name__ == "__main__":
-    root_folder = "F:/Dataset-2021-sbl/2021-09-02-sbl-cloutier-z3-P4RTK-WGS84"
+    # Read the CSV file containing folder paths
+    csv_file = "filenamesrgb_TS_lefodata.csv"
+    df = pd.read_csv(csv_file)
+    
+    # Create a list to store results
+    results = []
+    
+    # Process each folder path in the DataFrame
+    for index, row in df.iterrows():
+        folder_path = row["Folder Path"]
+        print(f"Processing folder: {folder_path}")
+        earliest, latest = process_folder(folder_path)
+        
+        if earliest and latest:
+            results.append({
+                "Folder Path": folder_path,
+                "Earliest Timestamp": earliest,
+                "Latest Timestamp": latest
+            })
+        else:
+            print(f"No JPEG files with valid timestamps found in {folder_path}")
+    
+    # Create a DataFrame from the results
+    results_df = pd.DataFrame(results)
+    
+    # Convert timestamps to RFC 3339 UTC format
+    results_df["Earliest Timestamp"] = pd.to_datetime(results_df["Earliest Timestamp"]).dt.strftime('%Y-%m-%dT%H:%M:%SZ')
+    results_df["Latest Timestamp"] = pd.to_datetime(results_df["Latest Timestamp"]).dt.strftime('%Y-%m-%dT%H:%M:%SZ')
+
+    # Save the results DataFrame to an Excel file
+    excel_output_file = "timestamps_rgb.xlsx"
+    results_df.to_excel(excel_output_file, index=False)
+    
+    print("Timestamps saved to", excel_output_file)
+
+if __name__ == "__main__":
+    root_folder = "F:/Dataset-2021-sbl/missions_drones/2021-09-02-sbl-cloutier-z3-P4RTK-WGS84"
     earliest, latest = process_folder(root_folder)
     
     if earliest and latest:
